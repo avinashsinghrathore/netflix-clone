576576c64d26298cc6f466146743a1d00b6eef34
avinash singh rathore <avinashrathore1.a@gmail.com>
2023-07-25 14:25:26 +0530

    netflix clone completed

---
+++ src/axios.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+// const cors = require('cors')
+
+
+
+// base url make the request to the movie database
+const instance = axios.create ({
+    baseURL: 'https://api.themoviedb.org/3',
+})
+
+export default instance
